0de78cac5aeb660f749646a0963f4fd423d89d89
NRothera <31472463+NRothera@users.noreply.github.com>
2017-11-02 18:29:31 +0000

    working reset button

--- script.js
+++ script.js
@@ -6,27 +6,35 @@ $(function(event){
     function changeTableCells(){
       $('td').each(function(index, value){
         $(this).click(function(event){
-
-
-
-
-
-
+          //This is player ones go, turns boxes black with an X
           if ((player1===true) && (!$(this).is('#clicked'))){
             $(this).addClass('X').html('X')
             player2 = true
             player1 = false
             $(this).attr('id', 'clicked');
-
+            $(".playerTurn").html("It is O's turn")
+          //This is player 2's go, turns boxes white with an O
           }else if((player2===true)&&(!$(this).is('#clicked'))){
             $(this).addClass('O').html('O')
             player1=true
             player2=false
             $(this).attr('id', 'clicked');
+            $(".playerTurn").html("It is X's turn")
           }
         })
       })
     }
+
+
+
+    //Function to clear everything
+    $('#reset').click(function(event){
+      $('td').each(function(index,value){
+        $(this).html(' ').css('background-color', 'none')
+        $(this).attr('id', '')
+        $(this).attr('class', '')
+      })
+    })
 
     changeTableCells()
 
@@ -40,6 +48,5 @@ $(function(event){
 //Function
 
 
-//Function to clear everything
 
 //Function to find out if anyone has won
